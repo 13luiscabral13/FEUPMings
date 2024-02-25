caa0f4fa87e98937de07c49384476d0b113e6cf4
Barbarafilipa <barbarafilipacarvalho08@gmail.com>
2024-02-25 09:39:56 +0000

    First commit

---
+++ Player/player.gd
@@ -0,0 +1,25 @@
+extends CharacterBody2D
+
+
+const SPEED = 300.0
+const JUMP_VELOCITY = -400.0
+
+# Get the gravity from the project settings to be synced with RigidBody nodes.
+var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+
+#Track the direction the lemming is moving in
+var move_direction = 1
+
+func _physics_process(delta):
+	# Add the gravity.
+	if not is_on_floor():
+		velocity.y += gravity * delta
+	
+	if is_on_wall():
+		print("Hit wall")
+		move_direction *= -1
+		#var collider = get_slide_collision(0).get_collider()
+
+	# Automatically move in the current direction
+	velocity.x = move_direction * SPEED
+	move_and_slide()
