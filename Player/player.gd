16f64fc9b04f33df92661262ecfd6996eb8a2f9b
Barbarafilipa <barbarafilipacarvalho08@gmail.com>
2024-02-25 12:24:47 +0000

    Primeiro lemming adicionado

--- Player/player.gd
+++ Player/player.gd
@@ -1,6 +1,5 @@
 extends CharacterBody2D
 
-
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
 
@@ -9,6 +8,8 @@ var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
 #Track the direction the lemming is moving in
 var move_direction = 1
+
+@onready var anim = get_node("AnimationPlayer")
 
 func _physics_process(delta):
 	# Add the gravity.
@@ -19,7 +20,12 @@ func _physics_process(delta):
 		print("Hit wall")
 		move_direction *= -1
 		#var collider = get_slide_collision(0).get_collider()
-
+	
+	if move_direction == -1:
+		get_node("AnimatedSprite2D").flip_h = true
+	else:
+		get_node("AnimatedSprite2D").flip_h = false
+		
 	# Automatically move in the current direction
 	velocity.x = move_direction * SPEED
 	move_and_slide()
